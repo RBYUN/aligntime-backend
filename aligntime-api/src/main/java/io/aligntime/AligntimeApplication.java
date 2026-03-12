8c265d2aa971e2e00232e4c926e2ef6ce2a308dc
Rodney Byun <rjbyun@gmail.com>
2026-03-11 23:59:02 -0400

    aligntime init

---
+++ aligntime-api/src/main/java/io/aligntime/AligntimeApplication.java
@@ -0,0 +1,11 @@
+package io.aligntime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AligntimeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AligntimeApplication.class, args);
+    }
+}
